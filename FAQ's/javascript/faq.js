9eda03401bc38caa61710078a26266efa5e4fcd2
agasheadwait <agasheadwait24@gmail.com>
2020-12-10 18:29:26 +0530

    Fixed mobile view and added navbar in FAQ page

--- FAQ's/javascript/faq.js
+++ FAQ's/javascript/faq.js
@@ -1,3 +1,9 @@
+const ham = document.getElementById("ham");
+const navtext = document.getElementById("nav-text");
+ham.onclick = () => {
+  navtext.classList.toggle("active");
+};
+
 function handleclick(value) {
   let questionDiv = document.getElementById("q" + value);
   if (questionDiv.classList.contains("faq__question__visible")) {
